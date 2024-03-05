fb95a149190c7a63b6e75bf2373f106fdaddf36a
Ryan Chen <y.f.chenroderick@gmail.com>
2024-03-05 18:59:34 +0800

    feat: 新增 logo 與網頁主形象

---
+++ components/LogoNav.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex items-center gap-x-2">
+    <img
+      src="~/assets/images/logo.svg"
+      alt="後台系統模擬"
+      width="22"
+      height="22"
+    />
+    <UBadge color="cyan" variant="outline">後台系統模擬</UBadge>
+  </div>
+</template>
+
+<style scoped></style>
