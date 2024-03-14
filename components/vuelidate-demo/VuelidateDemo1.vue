aa22237d11151d77e774efc01090a421ee140365
Ryan Chen <y.f.chenroderick@gmail.com>
2024-03-14 16:26:10 +0800

    feat: 建立基礎使用 vuelidate

--- components/vuelidate-demo/VuelidateDemo1.vue
+++ components/vuelidate-demo/VuelidateDemo1.vue
@@ -1,12 +1,34 @@
-<script setup>
-const obj = reactive({
-  name: 'Ryan',
-  age: 36,
-  job: 'student'
+<script lang="ts" setup>
+import schemaJSON from '~/data/dev-schema-vuelidate-simple.json';
+// schema在最上層就將他加入響應式系統
+import { useVuelidate } from '@vuelidate/core';
+import traverseSchemaToRules from '~/utils/traverseSchemaToRules';
+import traverseSchemaToState from '~/utils/traverseSchemaToState';
+
+const rawSchema = JSON.parse(JSON.stringify(schemaJSON));
+const schema = reactive(rawSchema);
+const rules = traverseSchemaToRules(rawSchema);
+const state = reactive(traverseSchemaToState(schema));
+
+console.log('state', state);
+console.log('rules', rules);
+const v$ = useVuelidate(rules, state, {
+  // Tells Vuelidate to track changes on the state automatically.
+  $autoDirty: true,
+  // When set to false, tells the validation rules to be called on init, otherwise they are lazy and only called when the field is dirty.
+  $lazy: true
 });
 </script>
 <template>
   <div class="border border-primary rounded-md p-2">
     <h1>Vuelidate Demo 1</h1>
+    <UInput v-model="state.basicJSON.title" />
+    <span class="text-rose" v-if="v$.basicJSON.title.required.$invalid">{{
+      v$.basicJSON.title.required.$message
+    }}</span>
+    <h2>state:</h2>
+    <pre>{{ state }}</pre>
+    <h2>v$:</h2>
+    <pre>{{ v$ }}</pre>
   </div>
 </template>
