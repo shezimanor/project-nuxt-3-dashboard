535ec1991d54cffa6cb84dd2e7be7343720742f5
Ryan Chen <y.f.chenroderick@gmail.com>
2024-03-22 00:39:36 +0800

    feat: 新增欄位元件 `MySchemaRange`

---
+++ components/my-schema/field/MySchemaRange.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+// props
+const props = defineProps({
+  schema: {
+    type: Object,
+    default: null
+  },
+  state: {
+    type: [Array, String, Number, Boolean],
+    default: undefined
+  },
+  paths: {
+    type: Array,
+    default: () => []
+  }
+});
+// 輸入值
+const modelValue = ref(getTypeDefault(props.schema.type));
+modelValue.value = props.state;
+
+// 注入依賴: rootState(這裡沒用到) 和更新用 action: updateState
+const { rootState, updateState } = inject('rootState') as {
+  [key: string]: any;
+};
+
+// 元件設定
+const defaultConfig = ref({
+  disabled: false,
+  min: 0,
+  max: 100,
+  step: 1
+});
+const mergedConfig = computed(() => {
+  // target 物件會更新(first params)
+  return Object.assign(defaultConfig.value, props.schema.ui.widgetConfig);
+});
+
+// modelValue 更新後，
+watch(modelValue, (newValue) => {
+  updateState(props.paths, newValue);
+});
+</script>
+
+<template>
+  <UFormGroup :label="schema.ui.label">
+    <div class="flex flex-row items-center gap-x-2">
+      <!-- <UInput
+        v-model.number="modelValue"
+        readonly
+        class="w-12 text-center grow-0 shrink-0"
+      /> -->
+      <UBadge
+        color="white"
+        variant="outline"
+        size="md"
+        class="w-12 justify-center grow-0 shrink-0"
+        >{{ modelValue }}</UBadge
+      >
+      <URange
+        v-model.number="modelValue"
+        :disabled="mergedConfig.disabled"
+        :min="mergedConfig.min"
+        :max="mergedConfig.max"
+        :step="mergedConfig.step"
+        class="grow"
+      />
+    </div>
+  </UFormGroup>
+</template>
+
+<style scoped></style>
