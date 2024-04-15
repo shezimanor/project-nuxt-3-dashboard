d6a9bc62e1b2c93a90091479742d65113fef2277
Ryan Chen <y.f.chenroderick@gmail.com>
2024-04-15 22:31:46 +0800

    feat: 「產品展示頁面」移除靜態資料，完成使用實際的資料引用並渲染對應的原型預覽

--- pages/product/showcase/[id].vue
+++ pages/product/showcase/[id].vue
@@ -1,71 +1,40 @@
 <script lang="ts" setup>
-const prototypeClassName = ref(
-  'border w-[258px] h-[450px] overflow-hidden sm:w-[344px] sm:h-[600px]'
+import {
+  MyPrototypeSimple,
+  MyPrototypeSwiper,
+  MyPrototypeUnknown
+} from '#components';
+import type { Product } from '~/types';
+
+const $route = useRoute();
+// `useLazyAsyncData` 使用這個函數來取得資料，導航不會等待資料回應，會自行跑完導航作業，才不會有閒置頁面產生，`useAsyncData` 反之
+const { data: productData, pending } = await useLazyAsyncData(
+  'getProduct',
+  () => $fetch(`/api/products/${$route.params.id}`)
 );
-const productStateForSwiper = ref({
-  basicData: {
-    title: '旋轉立方體 Demo1',
-    description: '這是一個旋轉立方體的 Demo'
-  },
-  prototypeData: {
-    loop: true,
-    direction: 'horizontal',
-    buttonText: '立即購買',
-    buttonColor: 'orange',
-    slides: [
-      {
-        title: '白色運動鞋',
-        description: '',
-        image: 'white.webp',
-        url: 'https://www.example.com/shoes/1'
-      },
-      {
-        title: '棕色登山鞋',
-        description: '復刻經典',
-        image: 'brown.webp',
-        url: 'https://www.example.com/shoes/2'
-      },
-      {
-        title: '象牙色球鞋',
-        description: '環保材質 | 限量發售',
-        image: 'ivory.webp',
-        url: 'https://www.example.com/shoes/3'
-      },
-      {
-        title: '紅色籃球鞋',
-        description: 'NBA球星最愛',
-        image: 'red.webp',
-        url: 'https://www.example.com/shoes/4'
-      },
-      {
-        title: '黃色籃球鞋',
-        description: '購物狂歡節限定款',
-        image: 'yellow.webp',
-        url: 'https://www.example.com/shoes/5'
-      }
-      // {
-      //   title: '黑色休閒鞋',
-      //   description: '輕巧材質 | 好穿耐磨',
-      //   image: 'black.webp',
-      //   url: 'https://www.example.com/shoes/6'
-      // }
-    ]
+const prototypeClassName = ref(
+  'relative border w-[258px] h-[450px] overflow-hidden sm:w-[344px] sm:h-[600px]'
+);
+
+//
+const ProductComponent = computed(() => {
+  if (!productData) return MyPrototypeUnknown;
+  const targetData = productData.value as Product;
+  // 依據 prototype_id 來決定要渲染的 Component
+  switch (targetData.prototype_id) {
+    case 'e415c8bd-a256-447f-a33a-9fbd7c3c06be':
+      return MyPrototypeSimple;
+    case '4dc207f2-2229-451a-aa9d-e29bb44acf84':
+      return MyPrototypeSwiper;
+    default:
+      return MyPrototypeUnknown;
   }
 });
-const productState = ref({
-  basicData: {
-    title: '靜態展示 Demo1',
-    description: '這是一個靜態展示 Demo'
-  },
-  prototypeData: {
-    title: '栗色福樂鞋',
-    description: '2024夏季新款',
-    image: 'maroon.webp',
-    buttonText: '立即購買',
-    buttonColor: 'yellow',
-    position: 'bottom', // top/bottom
-    url: 'https://www.example.com/shoes/maroon'
-  }
+const currentPrototypeData = computed(() => {
+  if (!productData) return null;
+  else
+    return JSON.parse((productData.value as Product).product_data)
+      .prototypeData;
 });
 </script>
 
@@ -79,14 +48,11 @@ const productState = ref({
       </UDashboardNavbar>
       <UDashboardPanelContent class="flex items-center justify-center">
         <!-- 依據使用的 Prototype 來渲染對應的 Component -->
-        <MyPrototypeSimple
+        <ProductComponent
+          v-if="productData && !pending && currentPrototypeData"
           :class="prototypeClassName"
-          :prototype-data="productState.prototypeData"
+          :prototype-data="currentPrototypeData"
         />
-        <!-- <MyPrototypeSwiper
-          :class="prototypeClassName"
-          :prototype-data="productStateForSwiper.prototypeData"
-        /> -->
       </UDashboardPanelContent>
     </UDashboardPanel>
   </UDashboardPage>
